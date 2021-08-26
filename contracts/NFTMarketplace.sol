062c5fc8393ceef246db5249074213fbd864da69
Rafael <rafael@molina-aquino.com>
2021-08-25 20:30:55 -0400

    Add comments for contract and js file

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -7,12 +7,35 @@ import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 
 /// @title NFT Marketplace
 /// @author AboveZtars
-/* /// @notice 
-/// @dev */
-contract NFTMarketplace is Initializable,OwnableUpgradeable  {
+/// @notice 
+/// @dev NFT Marketplace is inheriting from OwnableUpgradeable and OwnableUpgradeable is inheriting from Initializable, then NFTMarketplace is Initializable
+contract NFTMarketplace is OwnableUpgradeable  {
+    uint public fee;
+    address public recipient;
 
-    function initialize() public initializer {
-        __Ownable_init_unchained();
+    /// @dev Set initial values for our contract
+    function initialize(uint _fee, address _recipient) public initializer {
+        __Ownable_init();
+        fee = _fee;
+        recipient=_recipient;
+    }
+    ///@notice Only the owner can set the fee
+    ///@param _fee The fee to be set
+    ///@dev Pass the fee from testing script
+    function setFee(uint _fee) public onlyOwner {
+        fee = _fee;
+    }
+    ///@notice Only the owner can set the recipient
+    ///@param _recipient The recipient to be set
+    ///@dev Pass the recipient from testing script
+    function setRecipient(address _recipient) public onlyOwner {
+        recipient = _recipient;
     }
     
+    function sum() pure public returns(uint){
+        uint a = 2;
+        uint b = 2;
+        uint c = SafeMathUpgradeable.add(a,b);
+        return c;
+    }
 }
--- test/test.js
+++ test/test.js
@@ -3,6 +3,9 @@
 // Load dependencies
 const { expect } = require("chai");
 const { upgrades } = require("hardhat");
+// Variables for testing the settings of the variable inside the smart contract
+const fee = 2; 
+const recipient = 1; 
 
 // Start test block 1
 describe("NFT Marketplace without upgrades", function () {
@@ -10,41 +13,89 @@ describe("NFT Marketplace without upgrades", function () {
   let accounts;
 
   before('deploy NFT Marketplace', async () => {
+    accounts = await ethers.getSigners();
     console.log("Deploying NFT smart contract")
     const NFTMarketplaceFactory = await ethers.getContractFactory("NFTMarketplace");
     NFTMarketplace = await NFTMarketplaceFactory.deploy();
-    accounts = await ethers.getSigners();
-
     await NFTMarketplace.deployed();
   })
-  
-  // Test case for setting the right owner 
-  it("Checking owner without upgrades", async function () {
-    await NFTMarketplace.initialize();
-    var admin = await NFTMarketplace.owner(); 
-    console.log("Admin or owner of the contract: "+ admin);
-    expect(admin).to.equal(accounts[0].address);
+
+  // Test case for owner initial state
+  it("Checking initial state without upgrades", async function () {
+    //Initial state is 1% fee and address in position 1 of hardhat accounts
+    await NFTMarketplace.initialize(1,accounts[1].address); 
+    //Responses of variables inside the contract
+    let admin = await NFTMarketplace.owner();
+    let feeResponse = await NFTMarketplace.fee();
+    let recipientResponse = await NFTMarketplace.recipient();  
+    //Expect block to verify all 3 variables
+    expect(admin).to.equal(accounts[0].address); //Verifying the owner
+    expect(feeResponse).to.equal(1); //Verifying 1% fee
+    expect(recipientResponse).to.equal(accounts[1].address); // Verifying first recipient
   });
+  // Test case for setting the fee
+  it("Setting the Fee", async function () {
+    await NFTMarketplace.setFee(fee); 
+    let feeResponse = await NFTMarketplace.fee();
+    console.log("New fee: "+ feeResponse);
+    expect(feeResponse).to.equal(fee);
+  });
+
+  // Test case for setting the recipient
+  it("Setting the recipient", async function () {
+    await NFTMarketplace.setRecipient(accounts[recipient].address);
+    let recipientResponse = await NFTMarketplace.recipient();
+    console.log("New recipient: "+ recipientResponse);
+    expect(recipientResponse).to.equal(accounts[recipient].address);
+  });
+
+ /*  it("Checking Sum", async function () {
+    
+    var number = await NFTMarketplace.sum(); 
+    console.log("Number : "+ number);
+    expect(number).to.equal(4);
+  }); */
+
 });
 
 
-// Start test block 2
+/* // Start test block 2
 describe("NFT Marketplace with upgrades", function () {
   let NFTMarketplace;
   let accounts;
 
   before('deploy NFT Marketplace with upgrades', async () => {
     console.log("Deploying NFT smart contract")
-    const NFTMarketplaceFactory = await ethers.getContractFactory("NFTMarketplace");
-    NFTMarketplace = await upgrades.deployProxy(NFTMarketplaceFactory,[],{initializer: 'initialize'}); 
     accounts = await ethers.getSigners();
-  
+    const NFTMarketplaceFactory = await ethers.getContractFactory("NFTMarketplace");
+    NFTMarketplace = await upgrades.deployProxy(NFTMarketplaceFactory,[1,accounts[recipient].address],{initializer: 'initialize'}); 
     await NFTMarketplace.deployed();
   })
+  // Test case for setting the right owner 
   it("Checking owner with upgrades", async function () {
-    var admin = await NFTMarketplace.owner(); 
-    console.log("Admin or owner of the contract: "+ admin);
-    expect(admin).to.equal(accounts[0].address);
+    let admin = await NFTMarketplace.owner();
+    let feeResponse = await NFTMarketplace.fee();
+    let recipientResponse = await NFTMarketplace.recipient();  
+    //Expect block to verify all 3 variables
+    expect(admin).to.equal(accounts[0].address); //Verifying the owner
+    expect(feeResponse).to.equal(1); //Verifying 1% fee
+    expect(recipientResponse).to.equal(accounts[1].address); // Verifying first recipient
+  });
+
+  // Test case for setting the fee
+  it("Setting the Fee", async function () {
+    await NFTMarketplace.setFee(fee); 
+    let feeResponse = await NFTMarketplace.fee();
+    console.log("New fee: "+ feeResponse);
+    expect(feeResponse).to.equal(fee);
+  });
+
+  // Test case for setting the recipient
+  it("Setting the recipient", async function () {
+    await NFTMarketplace.setRecipient(accounts[recipient].address);
+    let recipientResponse = await NFTMarketplace.recipient();
+    console.log("New recipient: "+ recipientResponse);
+    expect(recipientResponse).to.equal(accounts[recipient].address);
   });
 });
-
+ */
