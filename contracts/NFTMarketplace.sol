d17f4ec2c2602c286c90349faaa1b3988bc58e07
Rafael <rafael@molina-aquino.com>
2021-08-26 20:28:38 -0400

    Create market offer

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,19 +1,60 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 
 /// @title NFT Marketplace
 /// @author AboveZtars
 /// @notice
 /// @dev NFT Marketplace is inheriting from OwnableUpgradeable and OwnableUpgradeable is inheriting from Initializable, then NFTMarketplace is Initializable
-contract NFTMarketplace is OwnableUpgradeable {
+contract NFTMarketplace is OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    
     uint256 public fee;
     address public recipient;
+
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    CountersUpgradeable.Counter private _offerIds; //Keep track of the offers in our market
+    CountersUpgradeable.Counter private _offersEnded; // Keep track of the ended offers in our market
+
+    ///VARIABLES
+    ///@param offerId The Id of the offer in the marketplace
+    ///@param nftContract The address of the nft token
+    ///@param tokenId The Id of the nft token
+    ///@param _amount The amount of nft token items to sell
+    ///@param _deadline The limit date to complete the offer
+    ///@param owner The owner of the nft token
+    ///@param price The price of the offer
+    ///@param offerExist Boolean parameter to know when the offer exist
+    struct MarketOffer {
+        uint offerId;
+        address nftContract;
+        uint256 tokenId;
+        uint _amount; 
+        uint _deadline; 
+        address payable owner;
+        uint256 price;
+        bool offerExist;
+    }
+
+    mapping(uint256 => MarketOffer) private idToMarketOffer;
+
+    ///EVENTS
+    event ItemOfferCreated (
+        uint indexed offerId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        uint _amount, 
+        uint _deadline,
+        address payable owner,
+        uint256 price,
+        bool offerExist
+    );
 
     /// @dev Set initial values for our contract
     function initialize(uint256 _fee, address _recipient) public initializer {
@@ -46,12 +87,58 @@ contract NFTMarketplace is OwnableUpgradeable {
     function getLatestPrice(address _pair) public view returns (int256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(_pair);
         (
-            uint80 roundID,
+            ,
             int256 price,
-            uint256 startedAt,
-            uint256 timeStamp,
-            uint80 answeredInRound
+            ,
+            ,
+            
         ) = priceFeed.latestRoundData();
         return price;
     }
+
+    /* Places an item for sale on the marketplace */
+  function createMarketOffer(
+    address _nftContract,
+    uint _tokenId, 
+    uint _amount, 
+    uint _deadline, 
+    uint _price
+  ) public payable nonReentrant {
+    require(_price > 0, "Price must be at least 1 wei");
+
+    _offerIds.increment();
+    uint256 offerId = _offerIds.current();
+
+    ///@param offerId The Id of the offer in the marketplace
+    ///@param nftContract The address of the nft token
+    ///@param tokenId The Id of the nft token
+    ///@param _amount The amount of nft token items to sell
+    ///@param _deadline The limit date to complete the offer
+    ///@param owner The owner of the nft token
+    ///@param price The price of the offer
+    ///@param offerExist Boolean parameter to know when the offer Exist
+  
+    idToMarketOffer[offerId] =  MarketOffer(
+      offerId,
+      _nftContract,
+      _tokenId,
+      _amount,
+      _deadline,
+      payable(msg.sender),
+      _price,
+      true
+    );
+
+    emit ItemOfferCreated(
+      offerId,
+      _nftContract,
+      _tokenId,
+      _amount,
+      _deadline,
+      payable(msg.sender),
+      _price,
+      true
+    );
+  }
+
 }
